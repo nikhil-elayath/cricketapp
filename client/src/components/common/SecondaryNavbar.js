0d9940958e2754fbf59488117a9d1dad2528a78b
ankit <agupta.ag19@gmail.com>
2019-09-27 20:32:55 +0530

    tweek in secondary navbar added match summary wireframe and added react-flag-icon-css package

--- client/src/components/common/SecondaryNavbar.js
+++ client/src/components/common/SecondaryNavbar.js
@@ -2,39 +2,43 @@ import React, { Component } from "react";
 import "../css/SecondaryNavbar.css";
 import dhoni from "../images/dhoni.jpg";
 import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
 
 export default class SecondaryNavbar extends Component {
 	render() {
 		return (
-			<div className="secNavParent">
-				<div className="secNavHead">
-					<div
-						style={{
-							backgroundImage: `url(${dhoni})`
-						}}
-					></div>
-					<div>
-						<h2>MS Dhoni</h2>
-						<h4>India</h4>
+			<>
+				<Navbar />
+				<div className="secNavParent">
+					<div className="secNavHead">
+						<div
+							style={{
+								backgroundImage: `url(${dhoni})`
+							}}
+						></div>
+						<div>
+							<h2>MS Dhoni</h2>
+							<h4>India</h4>
+						</div>
+					</div>
+					<div className="secNavLinks">
+						<ul>
+							<Link className="secLink" to="/info">
+								<li>Info</li>
+							</Link>
+							<Link className="secLink" to="/battingStats">
+								<li>Batting Stats</li>
+							</Link>
+							<Link className="secLink" to="/bowlingStats">
+								<li>Bowling Stats</li>
+							</Link>
+							<Link className="secLink" to="/performance">
+								<li>Performance</li>
+							</Link>
+						</ul>
 					</div>
 				</div>
-				<div className="secNavLinks">
-					<ul>
-						<Link className="secLink" to="/info">
-							<li>Info</li>
-						</Link>
-						<Link className="secLink" to="/battingStats">
-							<li>Batting Stats</li>
-						</Link>
-						<Link className="secLink" to="/bowlingStats">
-							<li>Bowling Stats</li>
-						</Link>
-						<Link className="secLink" to="/performance">
-							<li>Performance</li>
-						</Link>
-					</ul>
-				</div>
-			</div>
+			</>
 		);
 	}
 }
