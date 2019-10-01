2585c1c7e940e9b15e23305d46196a6fbf590dd7
Yatin Kalra <yatin.kalra22@gmail.com>
2019-10-01 22:04:36 +0530

    committing all changes before pulling

--- client/src/components/MatchSummaryDetails.js
+++ client/src/components/MatchSummaryDetails.js
@@ -8,8 +8,8 @@ import { connect } from "react-redux";
 export class MatchSummaryDetails extends Component {
 
     componentDidMount() {
-        this.props.getmatchdetailbyId(this.props.location.state.matches.id);
-        console.log(this.props.location.state.matches.id)
+        // this.props.getmatchdetailbyId(this.props.location.state.matches.id);
+        // console.log(this.props.location.state.matches.id)
     }
 
     render() {
@@ -42,89 +42,101 @@ export class MatchSummaryDetails extends Component {
 
 
                 </div>
-                <div className="summary">
+                <div style={{ marginTop: 90 + "px" }}>
+                    <div className="topOfSummary">
+                        <div className="scoreCardSummary">
+                            <div className="title">
+                                <h3 style={{ margin: 0 + "px", textAlign: "left", padding: 10 + "px", color: "white" }} > Score Card Summary</h3>
+                            </div>
+                            <div>
+                                <span style={{ float: "left", margin: 16 + "px", fontSize: 25 + "px" }}>India</span>
+                            </div>
+                            <div className="players">
+                                <div className="playerLeft">
+                                    <div className="playerinfo">
+                                        <p>Sachin</p>
+                                        <p>Rohit</p>
+                                    </div>
 
-
-                    <div className="scoreborad-summary">
-                        <div className="summary-title">
-                            <span className="titles">Scorecard Summary</span>
-                        </div>
-                        <div>
-                            <div className="inside-scoreborad-summary">
-                                <span className="team-name"> South Africa</span>
-                                <hr />
-                                <div className="team-players">
-                                    <div className="team-players-left">
-                                        <div className="team-players-name" style={{ float: "left" }}>Temba Bavuma</div>
-                                        <div className="team-players-score" style={{ float: "left" }}>49 (43)</div>
-                                    </div>
-                                    <div className="team-players-right">
-                                        <div className="team-players-name">Temba Bavuma</div>
-                                        <div className="team-players-score">49 (43)</div>
-                                    </div>
-                                    <div className="team-players-left">
-                                        <div className="team-players-name">Temba Bavuma</div>
-                                        <div className="team-players-score">49 (43)</div>
-                                    </div>
-                                    <div className="team-players-right">
-                                        <div className="team-players-name-right">Temba Bavuma</div>
-                                        <div className="team-players-score-right">49 (43)</div>
+                                    <div className="playerScores">
+                                        <p>50 (20)</p>
+                                        <p>51 (20)</p>
                                     </div>
                                 </div>
-                                <div>
 
+                                <div className="playerRight">
+                                    <div className="playerinfo">
+                                        <p>Sachin</p>
+                                        <p>Rohit</p>
+                                    </div>
+
+                                    <div className="playerScores">
+                                        <p>50 (20)</p>
+                                        <p>51 (20)</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <span style={{ float: "left", margin: 16 + "px", fontSize: 25 + "px" }}>India</span>
+                            </div>
+                            <div className="players">
+                                <div className="playerLeft">
+                                    <div className="playerinfo">
+                                        <p>Sachin</p>
+                                        <p>Rohit</p>
+                                    </div>
+
+                                    <div className="playerScores">
+                                        <p>50 (20)</p>
+                                        <p>51 (20)</p>
+                                    </div>
                                 </div>
 
-                            </div>
-                        </div>
-                    </div>
-                    <div className="match-details">
-                        <div className="summary-title">
-                            <span className="titles">Match Details</span></div>
-                        <div>
-                            <div className="match-details-header">Venue</div>
-                            <div className="match-details-value">Punjab Cricket Association IS, Bindra Station, Mohali, Chandigardh</div>
-                            <div className="match-details-header">Toss</div>
-                            <div className="match-details-value">India, selected to filed</div>
-                            <div className="match-details-header">Player of the match</div>
-                            <div className="match-details-value">MS DhoniAnil Chaudtry, Cheddy</div>
-                            <div className="match-details-header">Umire</div>
-                            <div className="match-details-value">Anil Chaudtry, Cheddy</div>
-                        </div>
-                    </div>
-                    <div className='playing-XI'>
-                        <div className="summary-title">
-                            <span className="titles">Playing XI</span></div>
-                        <div className="team-playing-XI">
-                            <div className="first-team-players"> South Africa
-                                <div> Player 1 </div>
-                                <div> Player 2 </div>
-                                <div> Player 3 </div>
-                                <div> Player 4 </div>
-                                <div> Player 5 </div>
-                                <div> Player 6 </div>
-                                <div> Player 7 </div>
-                                <div> Player 8 </div>
-                                <div> Player 9 </div>
-                                <div> Player 10 </div>
-                                <div> Player 11 </div>
+                                <div className="playerRight">
+                                    <div className="playerinfo">
+                                        <p>Sachin</p>
+                                        <p>Rohit</p>
+                                    </div>
+
+                                    <div className="playerScores">
+                                        <p>50 (20)</p>
+                                        <p>51 (20)</p>
+                                    </div>
+                                </div>
                             </div>
 
-                            <div className="second-team-players"> India</div>
-                            <div> Player 1 </div>
-                            <div> Player 2 </div>
-                            <div> Player 3 </div>
-                            <div> Player 4 </div>
-                            <div> Player 5 </div>
-                            <div> Player 6 </div>
-                            <div> Player 7 </div>
-                            <div> Player 8 </div>
-                            <div> Player 9 </div>
-                            <div> Player 10 </div>
-                            <div> Player 11 </div>
+                        </div>
+                        <div></div>
+                        <div className="matchDetails">
+                            <div className="title">
+                                <h3 style={{ margin: 0 + "px", textAlign: "left", padding: 10 + "px", color: "white" }} >
+                                    Match Details</h3>
+                            </div>
+                            <div className="match-title"> Venue</div>
+                            <h3 className="match-values">BJjkjbmnbmnb,mb</h3>
+                            <div className="match-title">Toss</div>
+                            <h3 className="match-values">India, elextwd to bat</h3>
+                            <div className="match-title">Player of the match</div>
+                            <h3 className="match-values">MS Shoni</h3>
+                            <div className="match-title">Umpires</div>
+                            <h3 className="match-values">Anil</h3>
+                        </div>
+
+                        <div className="playing11">
+                            <div className="title">
+                                <h3 style={{ margin: 0 + "px", textAlign: "left", padding: 10 + "px", color: "white" }} >
+                                    Playing XI</h3>
+
+                            </div>
+                            <div className="team-name">
+                                dsad
+                            </div>
                         </div>
                     </div>
                 </div>
+
+
+
             </div >
         )
     }
