82f605111f275793b4a59cb1617b2407daecc724
ankit <agupta.ag19@gmail.com>
2019-10-01 15:55:52 +0530

    integrated login register and reset password component

--- server/index.js
+++ server/index.js
@@ -1,27 +1,27 @@
 const config = require("config");
 const express = require("express");
-const cricketalpha = require("./routes/apis/CricketAlpha");
+const user = require("./routes/apis/user");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const error = require("./middleware/error");
 const app = express();
 
-const cricketPostgresURL = config.get("cricketPostgresURL");
+const cricketPostgresURL = config.get("postgresURL");
 
 app.use(cors());
 
 app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
+	bodyParser.urlencoded({
+		extended: false
+	})
 );
 
-app.use("/api/cricketalpha/", cricketalpha);
+app.use("/api/cricketalpha/user", user);
 app.use(error);
 
 const port = process.env.port || 5000;
 if (process.env.NODE_ENV !== "test")
-  app.listen(port, () => console.log(`Server is listening on port ${port}`));
+	app.listen(port, () => console.log(`Server is listening on port ${port}`));
 
 module.exports = app;
--- server/test/database-test/postgres.test.js
+++ server/test/database-test/postgres.test.js
@@ -2,25 +2,25 @@ const pgp = require("pg-promise")();
 const db = pgp("postgres://postgres:root@localhost:5432/cricketalpha");
 
 describe("Testing Postgres DB after INIT", () => {
-  // test case for product_tableaa
-  it("should have a products_info table", async () => {
-    var data = await db.any("select * from users limit 1");
-    expect(data).toEqual(expect.any(Array));
-  });
-  it("should have a user_name column in user table", async () => {
-    var data = await db.any("select user_name from users limit 1");
-    expect(data).toEqual(expect.any(Array));
-  });
-  it("should have a user_password column in users table", async () => {
-    var data = await db.any("select user_password from users limit 1");
-    expect(data).toEqual(expect.any(Array));
-  });
-  it("should have a user_email column in users table", async () => {
-    var data = await db.any("select user_email from users limit 1");
-    expect(data).toEqual(expect.any(Array));
-  });
-  it("should have a isadmin column table", async () => {
-    var data = await db.any("select  isadmin from users limit 1");
-    expect(data).toEqual(expect.any(Array));
-  });
+	// test case for product_tableaa
+	it("should have a user table", async () => {
+		var data = await db.any("select * from users limit 1");
+		expect(data).toEqual(expect.any(Array));
+	});
+	it("should have a user_name column in user table", async () => {
+		var data = await db.any("select user_name from users limit 1");
+		expect(data).toEqual(expect.any(Array));
+	});
+	it("should have a user_password column in users table", async () => {
+		var data = await db.any("select user_password from users limit 1");
+		expect(data).toEqual(expect.any(Array));
+	});
+	it("should have a user_email column in users table", async () => {
+		var data = await db.any("select user_email from users limit 1");
+		expect(data).toEqual(expect.any(Array));
+	});
+	it("should have a isadmin column table", async () => {
+		var data = await db.any("select  isadmin from users limit 1");
+		expect(data).toEqual(expect.any(Array));
+	});
 });
