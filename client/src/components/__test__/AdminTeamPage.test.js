504bdd082cd5a80f554b917853c572856c63ed7e
piyush kumar <kinngpiyush7@gmail.com>
2019-10-19 13:51:02 +0530

    updated admin pages test

--- client/src/components/__test__/AdminTeamPage.test.js
+++ client/src/components/__test__/AdminTeamPage.test.js
@@ -3,117 +3,213 @@ import { shallow } from "enzyme";
 import { AdminTeamPage } from "../AdminTeamPage";
 // import store from "../../store";
 // import { Provider } from "react-redux";
-import { getTeams } from "../../actions/Admin";
+import { getAllTeams, getTeamSearch } from "../../actions/Admin";
 const adminteampage = jest.fn();
-let teams = [];
-
+const show = jest.fn();
+const SweetAlert = jest.fn();
+const push = jest.fn();
+let team = [{ team_name: "India", team_id: "1" }];
+const createTeam = jest.fn();
+const showError = jest.fn();
+const onRegister = jest.fn();
+const historyMock = { push: jest.fn() };
+let statemock = {
+  showError: false,
+  errorMessage: "Please!"
+};
 const wrapper = shallow(
   <AdminTeamPage
     adminteampage={adminteampage}
-    teams={teams}
-    getTeams={getTeams}
+    team={team}
+    createTeam={createTeam}
+    // teams={teams}
+    getAllTeams={getAllTeams}
+    getTeamSearch={getTeamSearch}
+    history={{ historyMock, push: () => {} }}
+    show={show}
+    SweetAlert={SweetAlert}
+    showError={showError}
+    state={statemock}
+    onRegister={onRegister}
   />
 );
 
 describe("test  Component", () => {
   it("render the component", () => {
-    // const wrapper = shallow(<Register register={register} />);
     expect(wrapper).toMatchSnapshot();
   });
   it("there should be a div", () => {
-    expect(wrapper.find("div").length).toBe(8);
+    expect(wrapper.find("div").length).toBe(13);
   });
-  // it("there should be a navbar", () => {
-  //   expect(wrapper.find("NavBar").length).toBe(1);
-  // });
-  it("there should be a h1", () => {
-    expect(wrapper.find("h1").length).toBe(2);
-    expect(
-      wrapper
-        .find("h1")
-        .at(0)
-        .text()
-    ).toBe("All Team");
-    expect(
-      wrapper
-        .find("h1")
-        .at(1)
-        .text()
-    ).toBe("Add New Team");
+  it("test changing state ", () => {
+    const e = { preventDefault: () => {} };
+    jest.spyOn(e, "preventDefault");
+    wrapper.find("#add-team").simulate("click", e);
+
+    expect(statemock.showError).toBe(false);
   });
+
+  // check for the heading text
+  it("should have heading text ", () => {
+    expect(wrapper.find("#heading-1").text()).toBe("All Team");
+  });
+
+  // check for the heading text
+  it("should have heading text ", () => {
+    expect(wrapper.find("#heading-2").text()).toBe("Add New Team");
+  });
+
+  // check for how many input field
   it("there should be a input", () => {
     expect(wrapper.find("input").length).toBe(2);
   });
-  it("there should be a buttons", () => {
-    expect(wrapper.find("button").length).toBe(1);
+
+  it("testing for search input field", () => {
+    // checks for the presence of search input field
+    expect(wrapper.find("#searchInput").length).toBe(1);
+
+    // checks placeholder of the input field
+    expect(wrapper.find("#searchInput").prop("placeholder")).toBe("Search"); // checks for the input value to be same as state while inserting text
+    const event = {
+      target: {
+        name: "Search",
+        value: "Australia"
+      }
+    };
+    wrapper.instance().onSearchInputChange(event);
+    expect(wrapper.state().searchString).toBe(event.target.value);
   });
-  // it("there should be a link", () => {
-  //   expect(wrapper.find("Link").length).toBe(2);
-  // });
+
+  it("there should be a buttons", () => {
+    expect(wrapper.find("button").length).toBe(3);
+  });
 
   it("there should be a fieldset tag", () => {
     expect(wrapper.find("fieldset").length).toBe(1);
   });
 
-  // it("there should be a p tag", () => {
-  //   expect(wrapper.find("p").length).toBe(1);
-  //   expect(wrapper.find("p").text()).toBe("Already have an account ?Login");
-  // });
-  it("there should be a button", () => {
-    expect(wrapper.find("button").text()).toBe("Add Team");
+  it("there should be a button name", () => {
+    expect(wrapper.find("#add-team").text()).toBe("Add Team");
+  });
+  it("there should be a button name for delete and edit", () => {
+    expect(wrapper.find("#deletebutton").text()).toBe("Delete");
+    expect(wrapper.find("#editbutton").text()).toBe("Edit");
   });
 
-  // it("should change the state of testClicked to true when test tab is clicked", () => {
-  //   const test = wrapper.find("button").at(0);
-  //   test.simulate("click");
-  //   expect(wrapper.state().onRegister).toBe(true);
-  // });
+  it("checks for delete function to be called", () => {
+    // expects onRegister to be called on clicking add-team link
+    wrapper.find("#deletebutton").simulate("click");
+    expect(wrapper.state().show).toBe(true);
+  });
+  it("checks for fuction call on edit button for history  push", () => {
+    // expects onRegister to be called on clicking add-team link
+    wrapper.find("#editbutton").simulate("click");
+    // expect(historyMock.push).toBeCalledWith("/login");
+  });
 
   it("there should be a span", () => {
     expect(wrapper.find("span").length).toBe(1);
   });
 
-  it("there should type inputs", () => {
-    expect(
-      wrapper
-        .find("input")
-        .at(0)
-        .prop("type")
-    ).toEqual("text");
-    expect(
-      wrapper
-        .find("input")
-        .at(1)
-        .prop("type")
-    ).toEqual("text");
+  it("there should type in inputs", () => {
+    expect(wrapper.find("#searchInput").prop("type")).toBe("text");
+    expect(wrapper.find("#team_name").prop("type")).toBe("text");
   });
 
   it("there should name in  inputs", () => {
-    expect(
-      wrapper
-        .find("input")
-        .at(0)
-        .prop("name")
-    ).toEqual("Search");
-    expect(
-      wrapper
-        .find("input")
-        .at(1)
-        .prop("name")
-    ).toEqual("team_name");
+    expect(wrapper.find("#searchInput").prop("name")).toBe("Search");
+    expect(wrapper.find("#team_name").prop("name")).toBe("team_name");
   });
-  it("there should name in  inputs", () => {
-    expect(
-      wrapper
-        .find("input")
-        .at(0)
-        .prop("placeholder")
-    ).toEqual("Search");
-    expect(
-      wrapper
-        .find("input")
-        .at(1)
-        .prop("placeholder")
-    ).toEqual("Enter Team Name");
+
+  it("testing for add team input field", () => {
+    // checks for the presence of search input field
+    expect(wrapper.find("#team_name").length).toBe(1);
+
+    // checks placeholder of the input field
+    expect(wrapper.find("#team_name").prop("placeholder")).toBe(
+      "Enter Team Name"
+    ); // checks for the input value to be same as state while inserting text
+    const event = {
+      target: {
+        name: "team_name",
+        value: "Australia"
+      }
+    };
+    wrapper.instance().OnChange(event);
+    expect(wrapper.state().team_name).toBe(event.target.value);
   });
+
+  it("should be ok when render component with store", () => {
+    expect(wrapper.find("SweetAlert").length).toBe(1);
+    expect(wrapper.find("SweetAlert").prop("show")).toBe(true);
+    expect(wrapper.find("SweetAlert").prop("title")).toBe("DELETE TEAM");
+    expect(wrapper.find("SweetAlert").prop("text")).toBe("Are you sure?");
+  });
+
+  it("checks for onEscapeKey to be called", () => {
+    // expects onRegister to be called on clicking add-team link
+    wrapper.find("SweetAlert").props("onEscapeKey");
+    expect(wrapper.state().show).toBe(true);
+    wrapper.find("SweetAlert").props("onOutsideClick");
+    expect(wrapper.state().show).toBe(true);
+  });
+
+  it("checks for componenetDidmMount  to be called", () => {
+    // expects onRegister to be called on clicking add-team link
+    const componentDidMount = jest.spyOn(
+      AdminTeamPage.prototype,
+      "componentDidMount"
+    );
+    wrapper.instance().componentDidMount();
+    expect(componentDidMount).toHaveBeenCalled();
+  });
+
+  it("input fields should be filled correctly", () => {
+    wrapper
+      .find("#team_name")
+      .simulate("change", { target: { name: "team_name", value: "" } });
+
+    expect(wrapper.state("team_name")).toEqual("");
+  });
+  it("testing for search input field if empty", () => {
+    // checks for the presence of search input field
+    expect(wrapper.find("#searchInput").length).toBe(1);
+
+    // checks placeholder of the input field
+    expect(wrapper.find("#searchInput").prop("placeholder")).toBe("Search"); // checks for the input value to be same as state while inserting text
+    const event = {
+      target: {
+        name: "Search",
+        value: ""
+      }
+    };
+    wrapper.instance().onSearchInputChange(event);
+    expect(wrapper.state().searchString).toBe("");
+  });
+  it("checks for onRegister function to be called", () => {
+    const e = { preventDefault: () => {} };
+    jest.spyOn(e, "preventDefault");
+    // expects onRegister to be called on clicking add-team link
+    wrapper.find("#add-team").simulate("click", e);
+    expect(e.preventDefault).toBeCalled();
+    // expect(onRegister).toBeCalled();
+  });
+  it("test changing state ", () => {
+    const e = { preventDefault: () => {} };
+    jest.spyOn(e, "preventDefault");
+    wrapper.find("#editbutton").simulate("click", e);
+
+    expect(statemock.showError).toBe(false);
+  });
+
+  // it("checks for onRegister function to be called", () => {
+  //   const e = { preventDefault: () => {} };
+  //   jest.spyOn(e, "preventDefault");
+  //   // expects onRegister to be called on clicking add-team link
+  //   expect(wrapper.find("#add-team").simulate("click", e));
+  //   //expect(e.preventDefault).toBeCalled();
+  //   // expect(onRegister).toBeCalled();
+  //   // jest.spyOn(e, "onRegister");
+  //   // expect(onRegister).toBeCalled();
+  // });
 });
--- client/src/reducers/__test__/AdminTeamReducer.test.js
+++ client/src/reducers/__test__/AdminTeamReducer.test.js
@@ -1,32 +1,34 @@
 import AdminTeam from "../AdminTeamReducer";
 import {
-  GET_TEAMS,
+  GET_ALLTEAMS,
   CREATE_TEAM,
   EDIT_TEAM,
-  DELETE_TEAM
+  DELETE_TEAM,
+  GET_TEAM_SEARCH,
+  ERROR_TYPE
 } from "../../actions/Types";
 
 describe("Testing Users Reducers", () => {
   // for get player
-  it("should return a state object with user array equal to the payload in the action when the action type is GET_USER (when the returned state is initial state)", () => {
+  it("should return a state object with team array equal to the payload in the action when the action type is GET_TEAMS (when the returned state is initial state)", () => {
     const action = {
-      type: GET_TEAMS,
+      type: GET_ALLTEAMS,
       payload: [{}, {}, {}]
     };
     const returnedState = AdminTeam(undefined, action);
-    expect(returnedState).toEqual({ matches: action.payload });
+    expect(returnedState).toEqual({ error: "", team: action.payload });
   });
 
-  it("should return a state object with user array equal to the payload in the action when the action type is GET_USER (when the returned state is not an initial state)", () => {
+  it("should return a state object with user array equal to the payload in the action when the action type is GET_TEAMS (when the returned state is not an initial state)", () => {
     const initialState = {
-      users: [1, 2, 3, 4, 5]
+      team: [1, 2, 3, 4, 5]
     };
     const action = {
-      type: GET_USERS,
+      type: GET_ALLTEAMS,
       payload: [{}, {}, {}]
     };
     const returnedState = AdminTeam(initialState, action);
-    expect(returnedState).toEqual({ matches: action.payload });
+    expect(returnedState).toEqual({ team: action.payload });
   });
 
   it("should return the initial state object when the action type is not mentioned or doesn't concern the reducer (when the returned state is initial state)", () => {
@@ -34,92 +36,134 @@ describe("Testing Users Reducers", () => {
       payload: [{}, {}, {}]
     };
     let returnedState = AdminTeam(undefined, action);
-    expect(returnedState).toEqual({ matches: [] });
+    expect(returnedState).toEqual({ error: "", team: [] });
     action = {
       type: "SOME_TYPE",
       payload: [{}, {}, {}]
     };
     returnedState = AdminTeam(undefined, action);
-    expect(returnedState).toEqual({ matches: [] });
+    expect(returnedState).toEqual({ error: "", team: [] });
   });
 
-  it("should return a state object with user array equal to the payload in the action when the action type is GET_USERS (when the returned state is not an initial state)", () => {
+  it("should return a state object with team array equal to the payload in the action when the action type is GET_TEAMS (when the returned state is not an initial state)", () => {
     const initialState = {
-      user: [1, 2, 3, 4, 5]
+      team: [1, 2, 3, 4, 5]
     };
     let action = {
       payload: [{}, {}, {}]
     };
     let returnedState = AdminTeam(initialState, action);
-    expect(returnedState).toEqual({ matches: initialState.user });
+    expect(returnedState).toEqual({ team: initialState.team });
     action = {
       type: "SOME_TYPE",
       payload: [{}, {}, {}]
     };
     returnedState = AdminTeam(initialState, action);
-    expect(returnedState).toEqual({ matches: initialState.user });
+    expect(returnedState).toEqual({ team: initialState.team });
   });
 
   //for create team
-  it("should return a state object with user array equal to the payload in the action when the action type is CREATE_USERS (when the returned state is initial state)", () => {
+  it("should return a state object with team array when the action type is CREATE_TEAM", () => {
     const action = {
       type: CREATE_TEAM
+    };
+    const returnedState = AdminTeam(undefined, action);
+    expect(returnedState).toEqual({ error: "", team: [] });
+  });
+  it("should return a state object with team array when the action type is CREATE_TEAM", () => {
+    const initialState = {
+      team: [1, 2, 3, 4, 5]
+    };
+    const action = {
+      type: CREATE_TEAM
+    };
+    const returnedState = AdminTeam(initialState, action);
+    expect(returnedState).toEqual({ team: initialState.team });
+  });
+
+  // for edit team
+  it("should return a state object with team array when the action type is EDIT_TEAM", () => {
+    const action = {
+      type: EDIT_TEAM
       // payload: [{}, {}, {}]
     };
     const returnedState = AdminTeam(undefined, action);
-    expect(returnedState).toEqual({ team: [] });
+    expect(returnedState).toEqual({ error: "", team: [] });
   });
-  it("should return a state object with user array equal to the payload in the action when the action type is CREATE_USERS (when the returned state is not an initial state)", () => {
+  it("should return a state object with team array when the action type is EDIT_TEAM", () => {
     const initialState = {
-      users: [1, 2, 3, 4, 5]
+      team: [1, 2, 3, 4, 5]
     };
     const action = {
-      type: CREATE_TEAM
+      type: EDIT_TEAM
       // payload: [{}, {}, {}]
     };
     const returnedState = AdminTeam(initialState, action);
-    expect(returnedState).toEqual({ team: initialState.users });
-  });
-
-  // for edit player
-  it("should return a state object with user array equal to the payload in the action when the action type is CREATE_USERS (when the returned state is initial state)", () => {
-    const action = {
-      type: EDIT_TEAM
-      // payload: [{}, {}, {}]
-    };
-    const returnedState = AdminPlayer(undefined, action);
-    expect(returnedState).toEqual({ team: [] });
-  });
-  it("should return a state object with user array equal to the payload in the action when the action type is CREATE_USERS (when the returned state is not an initial state)", () => {
-    const initialState = {
-      users: [1, 2, 3, 4, 5]
-    };
-    const action = {
-      type: EDIT_TEAM
-      // payload: [{}, {}, {}]
-    };
-    const returnedState = AdminPlayer(initialState, action);
-    expect(returnedState).toEqual({ team: initialState.users });
+    expect(returnedState).toEqual({ team: initialState.team });
   });
 
   // for delete player
-  it("should return a state object with user array equal to the payload in the action when the action type is CREATE_USERS (when the returned state is initial state)", () => {
+  it("should return a state object with team array and error when the action type is DELETE_TEAM", () => {
     const action = {
       type: DELETE_TEAM
       // payload: [{}, {}, {}]
     };
-    const returnedState = AdminPlayer(undefined, action);
-    expect(returnedState).toEqual({ team: [] });
+    const returnedState = AdminTeam(undefined, action);
+    expect(returnedState).toEqual({ error: "", team: [] });
   });
-  it("should return a state object with user array equal to the payload in the action when the action type is CREATE_USERS (when the returned state is not an initial state)", () => {
+  it("should return a state object with team array when the action type is DELETE_TEAM", () => {
     const initialState = {
-      users: [1, 2, 3, 4, 5]
+      team: [1, 2, 3, 4, 5]
     };
     const action = {
       type: DELETE_TEAM
       // payload: [{}, {}, {}]
     };
-    const returnedState = AdminPlayer(initialState, action);
-    expect(returnedState).toEqual({ team: initialState.users });
+    const returnedState = AdminTeam(initialState, action);
+    expect(returnedState).toEqual({ team: initialState.team });
+  });
+
+  // // for search
+  it("should return a state object with team array equal to the payload in the action when the action type is GET_TEAM_SEARCH (when the returned state is initial state)", () => {
+    const action = {
+      type: GET_TEAM_SEARCH,
+      payload: [{}, {}, {}]
+    };
+    const returnedState = AdminTeam(undefined, action);
+    expect(returnedState).toEqual({ error: "", team: action.payload });
+  });
+
+  it("should return a state object with user array equal to the payload in the action when the action type is GET_TEAM_SEARCH (when the returned state is not an initial state)", () => {
+    const initialState = {
+      team: [1, 2, 3, 4, 5]
+    };
+    const action = {
+      type: GET_TEAM_SEARCH,
+      payload: [{}, {}, {}]
+    };
+    const returnedState = AdminTeam(initialState, action);
+    expect(returnedState).toEqual({ team: action.payload });
+  });
+
+  // for error type
+  it("should return a state object with team array equal to the payload in the action when the action type is GET_TEAM_SEARCH (when the returned state is initial state)", () => {
+    const action = {
+      type: ERROR_TYPE,
+      payload: [{}, {}, {}]
+    };
+    const returnedState = AdminTeam(undefined, action);
+    expect(returnedState).toEqual({ error: action.payload, team: [] });
+  });
+
+  it("should return a state object with user array equal to the payload in the action when the action type is GET_TEAM_SEARCH (when the returned state is not an initial state)", () => {
+    const initialState = {
+      error: [1, 2, 3, 4, 5]
+    };
+    const action = {
+      type: ERROR_TYPE,
+      payload: [{}, {}, {}]
+    };
+    const returnedState = AdminTeam(initialState, action);
+    expect(returnedState).toEqual({ error: action.payload });
   });
 });
