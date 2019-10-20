cd2ab03b206c8683b8067680b9ac89f8b455af97
Yatin Kalra <yatin.kalra22@gmail.com>
2019-10-20 21:27:32 +0530

    success text cases for match scorecard component

--- client/src/components/MatchScoreDetails.js
+++ client/src/components/MatchScoreDetails.js
@@ -28,7 +28,7 @@ export class MatchScoreDetails extends Component {
                   className="top-container-scorecard"
                   style={{ borderRadius: "5px" }}
                 >
-                  <div
+                  <div id="scorecard-inning"
                     style={{
                       borderTopLeftRadius: "5px",
                       borderTopRightRadius: "5px",
@@ -45,13 +45,13 @@ export class MatchScoreDetails extends Component {
                       className="batsman-heading"
                       style={{ padding: "5px", fontWeight: "500" }}
                     >
-                      <div>Batsmen</div>
+                      <div id="batsmen-text">Batsmen</div>
                       <div></div>
-                      <div>R</div>
-                      <div>B</div>
-                      <div>4s</div>
-                      <div>6s</div>
-                      <div>SR</div>
+                      <div id="runs-text">R</div>
+                      <div id="ball-text">B</div>
+                      <div id="fours-text">4s</div>
+                      <div id="sixes-text">6s</div>
+                      <div id="SR-text">SR</div>
                     </div>
                     <div>
                       {match.batsman.map(batsman => (
@@ -59,19 +59,19 @@ export class MatchScoreDetails extends Component {
                           className="batsman-heading"
                           style={{ padding: "5px" }}
                         >
-                          <div>{batsman.striker_name}</div>
+                          <div id="batsmen-name">{batsman.striker_name}</div>
                           {batsman.wicket_type ?
-                            (<div>
+                            (<div id="how-out">
                               {batsman.wicket_type == "caught" ? "(c) " : batsman.wicket_type}
                               {batsman.fielder_name}{" "} (b) {" "}
                               {batsman.bowler_name}
                             </div>) : (<div> not out </div>)
                           }
-                          <div>{batsman.batsman_run}</div>
-                          <div>{batsman.ball_faced}</div>
-                          <div>{batsman.fours}</div>
-                          <div>{batsman.sixes}</div>
-                          <div> {batsman.striker_rate}</div>
+                          <div id="batsmen-run">{batsman.batsman_run}</div>
+                          <div id="batsmen-ball-faced">{batsman.ball_faced}</div>
+                          <div id="batsmen-fours">{batsman.fours}</div>
+                          <div id="batsmen-sixes">{batsman.sixes}</div>
+                          <div id="batsmen-strike-rate">{batsman.striker_rate}</div>
                         </div>
                       ))}
                     </div>
@@ -80,9 +80,8 @@ export class MatchScoreDetails extends Component {
                       className="extras-content"
                       style={{ fontWeight: "500", padding: "10px" }}
                     >
-                      <div>Extras</div>
-                      <div style={{ margin: "0 80px 0 0" }}>
-                        {" "}
+                      <div id="match-extras-text">Extras</div>
+                      <div id="match-extras-value" style={{ margin: "0 80px 0 0" }}>
                         {match.extra_total.map(total => total.extra_count)} extras
                     </div>
                     </div>
@@ -91,8 +90,8 @@ export class MatchScoreDetails extends Component {
                       className="total-content"
                       style={{ fontWeight: "500", padding: "5px", borderBottom: "none" }}
                     >
-                      <div>Total</div>
-                      <div style={{ margin: "0 40px 0 0" }}>
+                      <div id="total-text">Total</div>
+                      <div id="total-value" style={{ margin: "0 40px 0 0" }}>
                         {match.total_score.map(score => score.total_runs)} /
                       {match.total_score.map(score => score.total_wicket)}({" "}
                         {match.total_score.map(score => score.total_overs)} overs)
@@ -102,13 +101,13 @@ export class MatchScoreDetails extends Component {
                       className="bowler-heading"
                       style={{ padding: "5px", fontWeight: "500", borderTop: "none" }}
                     >
-                      <div>Bowler</div>
+                      <div id="bowler-text">Bowler</div>
                       <div></div>
-                      <div>O</div>
-                      <div>R</div>
-                      <div>W</div>
-                      <div>Extra</div>
-                      <div>Ecom</div>
+                      <div id="bowler-over-text">O</div>
+                      <div id="bowler-run-text">R</div>
+                      <div id="bowler-wicket-text">W</div>
+                      <div id="bowler-extras-text">Extra</div>
+                      <div id="bowler-ecom-text">Ecom</div>
                     </div>
                     <div>
                       {match.all_bowler.map(bowler => (
@@ -116,13 +115,13 @@ export class MatchScoreDetails extends Component {
                           className="bowler-heading"
                           style={{ padding: "5px" }}
                         >
-                          <div>{bowler.bowler_name}</div>
+                          <div id="bowler-name-value">{bowler.bowler_name}</div>
                           <div></div>
-                          <div>{bowler.total_over}</div>
-                          <div>{bowler.given_runs}</div>
-                          <div>{bowler.wicket_taken}</div>
-                          <div>{bowler.total_extras}</div>
-                          <div>{bowler.ecom}</div>
+                          <div id="bowler-overs-value">{bowler.total_over}</div>
+                          <div id="bowler-runs-value">{bowler.given_runs}</div>
+                          <div id="bowler-wicket-value">{bowler.wicket_taken}</div>
+                          <div id="bowler-extras-value">{bowler.total_extras}</div>
+                          <div id="bowler-ecom-value">{bowler.ecom}</div>
                         </div>
                       ))}
                     </div>
