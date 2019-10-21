0719ac728e38abab7ff0ba3556133d014ace03a9
Yatin Kalra <yatin.kalra22@gmail.com>
2019-10-21 13:31:08 +0530

    match stats compoenet ready

--- client/src/components/MatchStatsDetails.js
+++ client/src/components/MatchStatsDetails.js
@@ -6,14 +6,14 @@ import {
   getPieChartTwobyId,
 } from "../actions/Matches";
 import { connect } from "react-redux";
-import Loader from "react-loader-spinner";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+// import Loader from "react-loader-spinner";
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 export class MatchStatsDetails extends Component {
   componentDidMount() {
     // [yatin] calling indivisual stats api on component mount
     // this.props.getManhattanGraphbyId(this.props.match_id);
-    this.props.getPieChartOnebyId(this.props.match_id);
+    // this.props.getPieChartOnebyId(this.props.match_id);
     // this.props.getPieChartTwobyId(this.props.match_id);
   }
   render() {
@@ -38,14 +38,12 @@ export class MatchStatsDetails extends Component {
             />
           </div>
         ) : ( */}
-        <div style={{ marginTop: 240 + "px" }}>
-          <div className="top-container-stats" style={{ marginTop: 80 + "px" }}>
-            <div></div>
+        <div style={{ marginTop: 230 + "px" }}>
+          <div className="stats-main-container">
             <div id="manhattan-container" className="stats-container">
-              <div id="manhattan-title" className="top-title">
-                {" "}
+              <p id="manhattan-title" className="top-title">
                 Manhattan
-              </div>
+              </p>
               <div>
                 <iframe
                   className="stats-graph"
@@ -53,38 +51,49 @@ export class MatchStatsDetails extends Component {
                 />
               </div>
             </div>
-            <div></div>
-          </div>
-
-          <div className="top-container-stats" style={{ marginTop: 70 + "px" }}>
-            <div></div>
-            <div className="stats-container">
-              <div className="top-title"> Pie chart of players run</div>
-              <div>
-                <iframe
-                  className="stats-graph"
-                  src={this.props.match_stats_pie1.head}
-                />
+            <div className="two-pie-chart-container">
+              <div className="stats-container">
+                <p className="top-title"> Pie chart1 of players run</p>
+                <div>
+                  <iframe
+                    className="stats-graph"
+                    src={this.props.match_stats_pie1.head}
+                  />
+                </div>
+              </div>
+              <div className="stats-container">
+                <p className="top-title"> Pie chart2 of players run</p>
+                <div>
+                  <iframe
+                    className="stats-graph"
+                    src={this.props.match_stats_pie1.head2}
+                  />
+                </div>
               </div>
             </div>
-            <div></div>
-          </div>
-          <div className="top-container-stats" style={{ marginTop: 70 + "px" }}>
-            <div></div>
-            <div className="stats-container">
-              <div className="top-title"> Pie chart of bowlers wicket</div>
-              <div>
-                <iframe
-                  className="stats-graph"
-                  src={this.props.match_stats_pie2.piechartTwo}
-                />
+            <div className="two-pie-chart-container">
+              <div className="stats-container">
+                <p className="top-title"> Pie1 chart of bowlers wicket</p>
+                <div>
+                  <iframe
+                    className="stats-graph"
+                    src={this.props.match_stats_pie2.head}
+                  />
+                </div>
+              </div>
+              <div className="stats-container">
+                <p className="top-title"> Pie2 chart of bowlers wicket</p>
+                <div>
+                  <iframe
+                    className="stats-graph"
+                    src={this.props.match_stats_pie2.head2}
+                  />
+                </div>
               </div>
             </div>
-            <div></div>
           </div>
         </div>
-        {/* )} */}
-      </div>
+      </div >
     );
   }
 }
